d1a5963b8052774d0e9b43f5b2d49f82def07293
jvgiv <jvgiv12@gmail.com>
2019-04-18 17:10:19 -0400

    full functionality

--- src/components/QuoteGenerator.js
+++ src/components/QuoteGenerator.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getRandomQuotes } from '../actions/randomQuote'
+import axios from 'axios'
 
 class QuoteGenerator extends Component {
   state = {
-    character: ''
+    character: '',
+    quotes: [],
+    name: 'homer'
   }
 
   handleChange = e => {
@@ -15,19 +18,45 @@ class QuoteGenerator extends Component {
     })
   }
 
+
+  quoteRetrieval = event => {
+    event.preventDefault();
+    axios
+      .post(`https://simpson-says-backend.herokuapp.com/users/generate`, { genChar: this.state.name })
+      .then(res =>{ 
+        this.setState({
+          quotes: res.data
+        })
+        console.log(res)
+      })
+  }
+
   render() {
     return (
       <div>
         <form>
-            <input 
-              type="text"
+            <select 
               name="name"
-              placeholder="Character Name"
-              value={this.state.character}
-              onChange={this.handleChange}
-              />
-              <button>Generate Quote</button>
+              value={this.state.name}
+              onChange={this.handleChange}>
+              <option value="homer">Homer</option>
+              <option value="marge">Marge</option>
+              <option value="bart">Bart</option>
+              <option value="lisa">Lisa</option>
+              <option value="moe">Moe</option>
+              <option value="grampa">Grampa</option>
+              <option value="skinner">Skinner</option>
+
+              </select>
+              <button onClick={this.quoteRetrieval} >Generate Quote</button>
           </form>
+
+          {this.state.quotes.map(eachQuote => {
+           return( <div>
+            <p><strong>Character: </strong>{this.state.name}</p>
+            <p><strong>Quote: </strong>{eachQuote.quote}</p>
+            </div>)
+    })}
       </div>
     )
   }
